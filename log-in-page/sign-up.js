0029ecd1aa2f3af23ee91a7aaffc81c6d5a6b8a5
Zachary Jensen <zjensen1022@gmail.com>
2016-07-28 17:25:05 +0000

    changed styles and made a node server

--- log-in-page/sign-up.js
+++ log-in-page/sign-up.js
@@ -1,17 +1,47 @@
+var http = require('http');
+
 function test(){
-    var user = document.getElementById("user").value;
-    var pass = document.getElementById("pass").value;
-    var confirmPass = document.getElementById("confirmPass").value;
-    if(user.length < 4 || pass.length < 4){
-        alert("username or password too short");
-    }
-    if(pass == confirmPass){
-        module.exports.user = user;
-        module.exports.pass = pass;
-    }
-    else{
-        alert("password doesn't match");
+    
+    if(event.keycode){
+        
+        var user = document.getElementById("user").value;
+        var pass = document.getElementById("pass").value;
+        var confirmPass = document.getElementById("confirmPass").value;
+        if(user.length < 4 || pass.length < 4){
+            alert("username or password too short (has to be at least 4 characters)");
+        }
+        if(pass == confirmPass){
+            module.exports.user = user;
+            module.exports.pass = pass;
+        }
+        else{
+            alert("password doesn't match");
+        }
     }
     
 }
+
+function button(){
+    
+    var user = document.getElementById("user").value;
+        var pass = document.getElementById("pass").value;
+        var confirmPass = document.getElementById("confirmPass").value;
+        if(user.length < 4 || pass.length < 4){
+            alert("username or password too short (has to be at least 4 characters)");
+        }
+        if(pass == confirmPass){
+            module.exports.user = user;
+            module.exports.pass = pass;
+        }
+        else{
+            alert("password doesn't match");
+        }
+        
+}
+
+http.createServer(function(request, response){
+    
+    response.write("I hope this works");
+    
+}).listen(8080);
 
