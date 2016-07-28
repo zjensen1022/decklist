af9caf5afa05a2915a02f285fc646a6c1df239ac
Zachary Jensen <zjensen1022@gmail.com>
2016-07-28 17:54:45 +0000

    changes variables to global

--- log-in-page/sign-up.js
+++ log-in-page/sign-up.js
@@ -1,12 +1,13 @@
 var http = require('http');
-
+var fs = require('fs');
+var user, pass, confirmPass;
 function test(){
     
     if(event.keycode){
         
-        var user = document.getElementById("user").value;
-        var pass = document.getElementById("pass").value;
-        var confirmPass = document.getElementById("confirmPass").value;
+        user = document.getElementById("user").value;
+        pass = document.getElementById("pass").value;
+        confirmPass = document.getElementById("confirmPass").value;
         if(user.length < 4 || pass.length < 4){
             alert("username or password too short (has to be at least 4 characters)");
         }
@@ -23,9 +24,9 @@ function test(){
 
 function button(){
     
-    var user = document.getElementById("user").value;
-        var pass = document.getElementById("pass").value;
-        var confirmPass = document.getElementById("confirmPass").value;
+        user = document.getElementById("user").value;
+        pass = document.getElementById("pass").value;
+        confirmPass = document.getElementById("confirmPass").value;
         if(user.length < 4 || pass.length < 4){
             alert("username or password too short (has to be at least 4 characters)");
         }
@@ -40,8 +41,10 @@ function button(){
 }
 
 http.createServer(function(request, response){
-    
-    response.write("I hope this works");
-    
+    response.writeHead(200);
+    fs.readFile('home.html', function(error, contents){
+        response.write(contents);
+    })
+    response.end();
 }).listen(8080);
 
